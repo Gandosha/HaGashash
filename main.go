d31db6f8b8fccb57ef3d11ac73c5a10e3144420d
Gandosha <Gandosha@users.noreply.github.com>
2018-09-05 18:58:15 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -16,28 +16,64 @@ func main() {
 	interfacePtr := flag.String("interface", "nil", "Name of the interface to use (Required! Run ifconfig before HaGashash in order to choose one).")
 	hostPtr := flag.String("host", "nil", "Scan only this host (Type its IP address or domain name).")
 	subnetPtr := flag.Bool("subnet", false, "Discover alive hosts in your current subnet and scan them.")
+	subnetsPtr := flag.Bool("subnets", false, "Discover alive hosts all subnets and scan them.")
 	/*dnsPtr := flag.Bool("dns", false, "Locate non-contiguous IP space and hostnames against specified domain. (Type "true" or "false").")
 	nmap spoof
 	nmap decoy*/
 	flag.Parse()
 	switch {
 		case *interfacePtr == "nil":
-			fmt.Println("\n[!] Please specify an interface name. (Ex. -interface=lo)\n\n")	
+			fmt.Println("\n\n[!] Please specify an interface name. (Ex. -interface=lo)\n\n")	
 			flag.PrintDefaults()
 			fmt.Println("\n")
 			os.Exit(1)
 		case *projectNamePtr == "nil":
-			fmt.Println("\n[!] Please specify a name for the project. (Ex. -project=example.com)\n\n")	
+			fmt.Println("\n\n[!] Please specify a name for the project. (Ex. -project=example.com)\n\n")	
 			flag.PrintDefaults()
 			fmt.Println("\n")
 			os.Exit(1)
-		case *hostPtr == "nil" && *subnetPtr == false:
-			fmt.Println("\n[!] Please specify the target. (Ex. -host=example.com or -subnet=true)\n\n")	
+		case *hostPtr == "nil" && *subnetPtr == false && *subnetsPtr == false:
+			fmt.Println("\n\n[!] Please specify the target. (Ex. -host=example.com or -subnet=true)\n\n")	
 			flag.PrintDefaults()
 			fmt.Println("\n")
 			os.Exit(1)
-		case *interfacePtr != "nil" && *hostPtr == "nil" && *projectNamePtr != "nil" && *subnetPtr == true:
-			fmt.Println("\n[!] Starting to scan your subnet.\n")
+		case *interfacePtr != "nil" && *hostPtr == "nil" && *projectNamePtr != "nil" && *subnetPtr == false && *subnetsPtr == true:
+			fmt.Println("\n\n[!] Starting to scan all subnets.\n\n")
+			var targets []string
+			ip := cmd.WhatIsMyIP(*interfacePtr)
+			tars := cmd.AliveHostsInAllSubnets(targets, ip)
+			for i:= range tars {
+				var tarsTCPorts, tarsUDPorts []string
+				path := "/home/" + userEnvVar + "HaGashash_Projects/" + *projectNamePtr + "/" + strings.Trim(tars[i],"'$'\n'")
+				cmd.CreateDirIfNotExist(path)
+				cmd.NmapTCPScan(strings.Trim(tars[i],"'$'\n'"),path)	//TCP scan
+				path = "/home/" + userEnvVar + "HaGashash_Projects/" + *projectNamePtr + "/" + strings.Trim(tars[i],"'$'\n'") + "/TCPxml"
+				xmlFile, err := os.Open(path)
+				if err != nil {
+					fmt.Println(err)
+				}
+				bytes, _ := ioutil.ReadAll(xmlFile)
+				defer xmlFile.Close()
+				xml := string(bytes)
+				tarsTCPorts = cmd.PortExtractor(xml, tarsTCPorts)
+				path = "/home/" + userEnvVar + "HaGashash_Projects/" + *projectNamePtr + "/" + strings.Trim(tars[i],"'$'\n'")
+				cmd.NmapVulnScan(tars[i], tarsTCPorts, path, "TCP")	//TCP vuln scan
+				cmd.NmapUDPScan(strings.Trim(tars[i],"'$'\n'"),path)	//UDP scan
+				path = "/home/" + userEnvVar + "HaGashash_Projects/" + *projectNamePtr + "/" + strings.Trim(tars[i],"'$'\n'") + "/UDPxml"
+				xmlFile, err = os.Open(path)
+				if err != nil {
+					fmt.Println(err)
+				}
+				bytes, _ = ioutil.ReadAll(xmlFile)
+				defer xmlFile.Close()
+				xml = string(bytes)
+				tarsUDPorts = cmd.PortExtractor(xml, tarsUDPorts)
+				path = "/home/" + userEnvVar + "HaGashash_Projects/" + *projectNamePtr + "/" + strings.Trim(tars[i],"'$'\n'")
+				cmd.NmapVulnScan(tars[i], tarsUDPorts, path, "UDP")	//UDP vuln scan
+				cmd.SummaryMaker(path,tars[i])
+			}
+		case *interfacePtr != "nil" && *hostPtr == "nil" && *projectNamePtr != "nil" && *subnetPtr == true && *subnetsPtr == false:
+			fmt.Println("\n\n[!] Starting to scan your subnet.\n\n")
 			var targets []string
 			ip := cmd.WhatIsMyIP(*interfacePtr)
 			tars := cmd.AliveHostsInSubnet(targets, ip)
@@ -69,9 +105,9 @@ func main() {
 				tarsUDPorts = cmd.PortExtractor(xml, tarsUDPorts)
 				path = "/home/" + userEnvVar + "HaGashash_Projects/" + *projectNamePtr + "/" + strings.Trim(tars[i],"'$'\n'")
 				cmd.NmapVulnScan(tars[i], tarsUDPorts, path, "UDP")	//UDP vuln scan
-				cmd.SummaryMaker(path,*hostPtr)
+				cmd.SummaryMaker(path,tars[i])
 			}
-		case *interfacePtr != "nil" && *hostPtr != "nil" && *projectNamePtr != "nil" && *subnetPtr == false:	
+		case *interfacePtr != "nil" && *hostPtr != "nil" && *projectNamePtr != "nil" && *subnetPtr == false && *subnetsPtr == false:	
 			//var tarsTCPorts, tarsUDPorts []string		//commented out for testing only tcp
 			var tarsTCPorts []string
 			path := "/home/" + userEnvVar + "HaGashash_Projects/" + *projectNamePtr + "/" + *hostPtr
