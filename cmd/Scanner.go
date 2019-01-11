6c1246542607b20377091ac366bdc917fecc84f3
Gandosha <Gandosha@users.noreply.github.com>
2019-01-11 17:47:54 -0500

    Add files via upload

--- cmd/Scanner.go
+++ cmd/Scanner.go
@@ -200,7 +200,7 @@ func WebScan(protocol string, targetIP string, outputPath string, port2scan stri
 	switch {
 		case fileInfo.Size() == 0:
 			color.Green("\n\n[!] Nikto initiated on: " + protocol + "://" + targetIP + ":" + port2scan + ".\n\n")
-			niktoCmd := exec.Command("bash", "-c", "nikto -h " + protocol + "://" + targetIP + ":" + port2scan + " -Tuning x12567 >> " + outputPath + "/nikto_scan_out_" + port2scan + "_" + protocol)
+			niktoCmd := exec.Command("bash", "-c", "nikto -h " + protocol + "://" + targetIP + ":" + port2scan + " >> " + outputPath + "/nikto_scan_out_" + port2scan + "_" + protocol)
 			err = niktoCmd.Run()
 		    	if err != nil {
 				panic(err)
@@ -214,7 +214,7 @@ func WebScan(protocol string, targetIP string, outputPath string, port2scan stri
 			//Initiate nikto with gobuster's output (Line_by_Line)
 			for scanner.Scan() {
 				color.Green("\n\n[!] Nikto initiated on: " + protocol + "://" + targetIP + ":" + port2scan + scanner.Text() + ".\n\n")
-				niktoCmd := exec.Command("bash", "-c", "echo -e '\n\n" + protocol + "://" + targetIP + ":" + port2scan + scanner.Text() + "\n\n' >> " + outputPath + "/nikto_scan_out_" + port2scan + "_" + protocol + " && nikto -h " + protocol + "://" + targetIP + ":" + port2scan + scanner.Text() + " -Tuning x12567 >> " + outputPath + "/nikto_scan_out_" + port2scan + "_" + protocol)
+				niktoCmd := exec.Command("bash", "-c", "echo -e '\n\n" + protocol + "://" + targetIP + ":" + port2scan + scanner.Text() + "\n\n' >> " + outputPath + "/nikto_scan_out_" + port2scan + "_" + protocol + " && nikto -h " + protocol + "://" + targetIP + ":" + port2scan + scanner.Text() + " >> " + outputPath + "/nikto_scan_out_" + port2scan + "_" + protocol)
 			    	err = niktoCmd.Run()
 			    	if err != nil {
 					panic(err)
